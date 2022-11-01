e2170cc28b09690d5174ff2dcdb0a5b8c6f347c7
Tanvi Agarwal <45534001+tanviagwl98@users.noreply.github.com>
2022-11-01 09:55:40 +0530

    portfolio updated

---
+++ tanvi_portfolio/src/components/About.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section id="about">
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+            Hi, I'm Tanvi Agarwal.</h1>
+            <h6 className="title-font sm:text-2xl text-3xl mb-4 font-medium text-white">
+            Software Developer & Content Writer
+          </h6>
+          <p className="mb-8 leading-relaxed">
+          I love building websites and applications. 
+          I also have passion to write content related 
+          to technical and non-technial fields.
+          </p>
+          <div className="flex justify-center">
+            <a
+              href="https://www.linkedin.com/in/tanvi-agarwal-70993b13a/"
+              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+              Work With Me
+            </a>
+            <a
+              href="#projects"
+              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+              See My Past Work
+            </a>
+          </div>
+        </div>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img
+            className="object-cover object-center rounded"
+            alt="hero"
+            src="./coding.svg"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
